7e0b0f776a1a1251fe00f9465edf28236bf3d467
jinfullliu <jinfullliu@tencent.com>
2024-03-28 20:39:17 +0800

    first commit

--- Ensemble_MixGCN.py
+++ Ensemble_MixGCN.py
@@ -9,59 +9,75 @@ def get_parser():
     parser.add_argument(
         '--ctrgcn_J2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_J/epoch1_test_score.pkl')
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_J/epoch1_test_score.pkl')
     parser.add_argument(
         '--ctrgcn_B2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_B/epoch1_test_score.pkl')
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_B/epoch1_test_score.pkl')
     parser.add_argument(
         '--ctrgcn_JM2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_JM/epoch1_test_score.pkl')
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_JM/epoch1_test_score.pkl')
     parser.add_argument(
         '--ctrgcn_BM2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_BM/epoch1_test_score.pkl')
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_BM/epoch1_test_score.pkl')
     parser.add_argument(
         '--ctrgcn_J3d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_J_3D/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_J_3D/epoch1_test_score.pkl'),
     parser.add_argument(
         '--ctrgcn_B3d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_B_3D/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_B_3D/epoch1_test_score.pkl'),
     parser.add_argument(
         '--ctrgcn_JM3d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_JM_3D/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_JM_3D/epoch1_test_score.pkl'),
     parser.add_argument(
         '--ctrgcn_BM3d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V2_BM_3D/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_BM_3D/epoch1_test_score.pkl'),
     parser.add_argument(
         '--tdgcn_J2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/tdgcn_V2_J/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/tdgcn_V1_J/epoch1_test_score.pkl'),
     parser.add_argument(
         '--tdgcn_B2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/tdgcn_V2_B/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/tdgcn_V1_B/epoch1_test_score.pkl'),
     parser.add_argument(
         '--tdgcn_JM2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/tdgcn_V2_JM/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/tdgcn_V1_JM/epoch1_test_score.pkl'),
     parser.add_argument(
         '--tdgcn_BM2d_Score', 
         type = str,
-        default = './Model_inference/Mix_GCN/output/tdgcn_V2_BM/epoch1_test_score.pkl'),
+        default = './Model_inference/Mix_GCN/output/tdgcn_V1_BM/epoch1_test_score.pkl'),
+    parser.add_argument(
+        '--mstgcn_J2d_Score', 
+        type = str,
+        default = './Model_inference/Mix_GCN/output/mstgcn_V1_J/epoch1_test_score.pkl'),
+    parser.add_argument(
+        '--mstgcn_B2d_Score', 
+        type = str,
+        default = './Model_inference/Mix_GCN/output/mstgcn_V1_B/epoch1_test_score.pkl'),
+    parser.add_argument(
+        '--mstgcn_JM2d_Score', 
+        type = str,
+        default = './Model_inference/Mix_GCN/output/mstgcn_V1_JM/epoch1_test_score.pkl'),
+    parser.add_argument(
+        '--mstgcn_BM2d_Score', 
+        type = str,
+        default = './Model_inference/Mix_GCN/output/mstgcn_V1_BM/epoch1_test_score.pkl'),
     parser.add_argument(
         '--val_sample', 
         type = str,
-        default = './Process_data/CS_test_V2.txt'),
+        default = './Process_data/CS_test_V1.txt'),
     parser.add_argument(
         '--benchmark', 
         type = str,
-        default = 'V2')
+        default = 'V1')
     return parser
 
 def Cal_Score(File, Rate, ntu60XS_num, Numclass):
@@ -114,25 +130,40 @@ if __name__ == "__main__":
     b3d_file = args.ctrgcn_B3d_Score
     jm3d_file = args.ctrgcn_JM3d_Score
     bm3d_file = args.ctrgcn_BM3d_Score
+    
     td_j_file = args.tdgcn_J2d_Score
     td_b_file = args.tdgcn_B2d_Score
     td_jm_file = args.tdgcn_JM2d_Score
     td_bm_file = args.tdgcn_BM2d_Score
     
+    mst_j_file = args.mstgcn_J2d_Score
+    mst_b_file = args.mstgcn_B2d_Score
+    mst_jm_file = args.mstgcn_JM2d_Score
+    mst_bm_file = args.mstgcn_BM2d_Score
+    
     val_txt_file = args.val_sample
 
-    File = [j_file, b_file, jm_file, bm_file, j3d_file, b3d_file, jm3d_file, bm3d_file, td_j_file, td_b_file, td_jm_file, td_bm_file]    
+    File = [j_file, b_file, jm_file, bm_file, 
+            j3d_file, b3d_file, jm3d_file, bm3d_file, 
+            td_j_file, td_b_file, td_jm_file, td_bm_file,
+            mst_j_file, mst_b_file, mst_jm_file, mst_bm_file]    
     if args.benchmark == 'V1':
         Numclass = 155
         Sample_Num = 6307
-        Rate = [0.7, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.7, 0.3, 0.3] 
+        Rate = [0.7, 0.7, 0.3, 0.3,
+                0.3, 0.3, 0.3, 0.3,
+                0.7, 0.7, 0.3, 0.3,
+                0.7, 0.7, 0.3, 0.3] 
         final_score = Cal_Score(File, Rate, Sample_Num, Numclass)
         true_label = gen_label(val_txt_file)
     
     if args.benchmark == 'V2':
         Numclass = 155
         Sample_Num = 6599
-        Rate = [0.7, 0.7, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7, 0.7, 0.3, 0.3] 
+        Rate = [0.7, 0.7, 0.3, 0.3,
+                0.3, 0.3, 0.3, 0.3,
+                0.7, 0.7, 0.3, 0.3,
+                0.05, 0.05, 0.05, 0.05] 
         final_score = Cal_Score(File, Rate, Sample_Num, Numclass)
         true_label = gen_label(val_txt_file)
     
