6f54b866efe05297002e96ac506fe1bbcb4d73ce
yyyybq <128017523+yyyybq@users.noreply.github.com>
2024-04-03 17:43:15 +0800

    Update Ensemble2.py

--- Ensemble2.py
+++ Ensemble2.py
@@ -20,8 +20,12 @@ def objective(weights):
         _, r88 = r8[i]
         _, r99 = r9[i]
         _, r1010 = r10[i]
+        _, r1111 = r11[i]
+        _, r1212 = r12[i]
+        _, r1313 = r13[i]
+        _, r1414 = r14[i]
         
-        r = r11 * weights[0] + r22 * weights[1] + r33 * weights[2] + r44 * weights[3] + r55 * weights[4] + r66 * weights[5]+ r77 * weights[6] + r88 * weights[7] + r99 * weights[8] + r1010 * weights[9]
+        r = r11 * weights[0] + r22 * weights[1] + r33 * weights[2] + r44 * weights[3] + r55 * weights[4] + r66 * weights[5]+ r77 * weights[6] + r88 * weights[7] + r99 * weights[8] + r1010 * weights[9]+ r1111 * weights[10]++ r1212 * weights[11]+ r1313 * weights[12]+ r1414 * weights[13]
         r = np.argmax(r)
         right_num += int(r == int(l))
         total_num += 1
@@ -55,20 +59,37 @@ if __name__ == "__main__":
                         help='Directory containing "epoch1_test_score.pkl" for bone eval results')
     parser.add_argument('--joint-motion-dir2', default=None)
     parser.add_argument('--bone-motion-dir2', default=None)
+    parser.add_argument('--ctrgcn_J3d_Score', default=None)
+    parser.add_argument('--ctrgcn_B3d_Score', default=None)
+    parser.add_argument('--ctrgcn_JM3d_Score', default=None)
+    parser.add_argument('--ctrgcn_BM3d_Score', default=None)
 
 
     arg = parser.parse_args()
 
     dataset = arg.dataset
+
+    '''-------------------------------------------'''
     if 'csv1' in arg.dataset:
-        npz_data = np.load('/data/liujinfu/icmew/pose_data/V1.npz')
+        npz_data = np.load('/data/liujinfu/icmew/pose_data/V1.npz')#set the path to the processed .npz
         label = npz_data['y_test']#np.where(npz_data['y_test'] > 0)[1]
     elif 'csv2' in arg.dataset:
-        npz_data = np.load('/data/liujinfu/icmew/pose_data/V2.npz')
+        npz_data = np.load('/data/liujinfu/icmew/pose_data/V2.npz')#set the path to the processed .npz
         label = npz_data['y_test']#np.where(npz_data['y_test'] > 0)[1]
 
     else:
         raise NotImplementedError
+
+    '''------------------------------------------'''
+    # another method to get label
+    '''
+    label = []
+    val_txt = np.loadtxt(val_txt_path, dtype = str)
+    for idx, name in enumerate(val_txt):
+        label1 = int(name.split('A')[1][:3])
+        label.append(label1)
+
+    label = torch.from_numpy(np.array(label))'''
 
     with open(os.path.join(arg.joint_dir, 'epoch1_test_score.pkl'), 'rb') as r1:
         r1 = list(pickle.load(r1).items())
@@ -102,9 +123,21 @@ if __name__ == "__main__":
     if arg.bone_motion_dir is not None:
         with open(os.path.join(arg.bone_motion_dir2, 'epoch1_test_score.pkl'), 'rb') as r10:
             r10 = list(pickle.load(r10).items())
+    if arg.ctrgcn_J3d_Score is not None:
+        with open(os.path.join(arg.ctrgcn_J3d_Score, 'epoch1_test_score.pkl'), 'rb') as r10:
+            r11 = list(pickle.load(r11).items())
+    if arg.ctrgcn_B3d_Score is not None:
+        with open(os.path.join(arg.ctrgcn_B3d_Score, 'epoch1_test_score.pkl'), 'rb') as r10:
+            r12 = list(pickle.load(r12).items())
+    if arg.ctrgcn_JM3d_Score is not None:
+        with open(os.path.join(arg.ctrgcn_JM3d_Score, 'epoch1_test_score.pkl'), 'rb') as r10:
+            r13 = list(pickle.load(r13).items())
+    if arg.ctrgcn_BM3d_Score is not None:
+        with open(os.path.join(arg.ctrgcn_BM3d_Score, 'epoch1_test_score.pkl'), 'rb') as r10:
+            r14 = list(pickle.load(r14).items())
             
     if arg.joint_motion_dir is not None and arg.bone_motion_dir is not None:
-        space = [(0.2, 1.2) for i in range(10)]
+        space = [(0.2, 1.2) for i in range(14)]#6+8 
         result = gp_minimize(objective, space, n_calls=200, random_state=0)
         print('Maximum accuracy: {:.4f}%'.format(-result.fun * 100))
         print('Optimal weights: {}'.format(result.x))
