a25dda006dd6915b15b7915d2c28577441732288
yyyybq <128017523+yyyybq@users.noreply.github.com>
2024-04-03 17:22:12 +0800

    Update Ensemble_MixFormer.py

--- Ensemble_MixFormer.py
+++ Ensemble_MixFormer.py
@@ -1,112 +1,113 @@
-import torch
-import pickle
 import argparse
+import pickle
+import os
+
 import numpy as np
-import pandas as pd
+from tqdm import tqdm
+from skopt import gp_minimize
 
-def get_parser():
-    parser = argparse.ArgumentParser(description = 'multi-stream ensemble') 
-    parser.add_argument(
-        '--mixformer_J_Score', 
-        type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_J/epoch1_test_score.pkl')
-    parser.add_argument(
-        '--mixformer_B_Score', 
-        type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_B/epoch1_test_score.pkl')
-    parser.add_argument(
-        '--mixformer_JM_Score', 
-        type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_JM/epoch1_test_score.pkl')
-    parser.add_argument(
-        '--mixformer_BM_Score', 
-        type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_BM/epoch1_test_score.pkl')
-    parser.add_argument(
-        '--mixformer_k2_Score', 
-        type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_J_3D/epoch1_test_score.pkl'),
-    parser.add_argument(
-        '--mixformer_k2M_Score', 
-        type = str,
-        default = './Model_inference/Mix_GCN/output/ctrgcn_V1_B_3D/epoch1_test_score.pkl'),
-    parser.add_argument(
-        '--val_sample', 
-        type = str,
-        default = './Process_data/CS_test_V1.txt')
-    parser.add_argument(
-        '--benchmark', 
-        type = str,
-        default = 'V1')
-    return parser
+def objective(weights):
+    right_num = total_num = 0
+    for i in tqdm(range(len(label))):
+        l = label[i]
+        _, r11 = r1[i]
+        _, r22 = r2[i]
+        _, r33 = r3[i]
+        _, r44 = r4[i]
+        _, r55 = r5[i]
+        _, r66 = r6[i]
+        _, r77 = r7[i]
+        _, r88 = r8[i]
+        _, r99 = r9[i]
+        _, r1010 = r10[i]
+        
+        r = r11 * weights[0] + r22 * weights[1] + r33 * weights[2] + r44 * weights[3] + r55 * weights[4] + r66 * weights[5]+ r77 * weights[6] + r88 * weights[7] + r99 * weights[8] + r1010 * weights[9]
+        r = np.argmax(r)
+        right_num += int(r == int(l))
+        total_num += 1
+    acc = right_num / total_num
+    print(acc)
+    return -acc  # We want to maximize accuracy, hence minimize -accuracy
 
-def Cal_Score(File, Rate, ntu60XS_num, Numclass):
-    final_score = torch.zeros(ntu60XS_num, Numclass)
-    for idx, file in enumerate(File):
-        fr = open(file,'rb') 
-        inf = pickle.load(fr)
-
-        df = pd.DataFrame(inf)
-        df = pd.DataFrame(df.values.T, index=df.columns, columns=df.index)
-        score = torch.tensor(data = df.values)
-        final_score += Rate[idx] * score
-    return final_score
-
-def Cal_Acc(final_score, true_label):
-    wrong_index = []
-    _, predict_label = torch.max(final_score, 1)
-    for index, p_label in enumerate(predict_label):
-        if p_label != true_label[index]:
-            wrong_index.append(index)
-            
-    wrong_num = np.array(wrong_index).shape[0]
-    print('wrong_num: ', wrong_num)
-
-    total_num = true_label.shape[0]
-    print('total_num: ', total_num)
-    Acc = (total_num - wrong_num) / total_num
-    return Acc
-
-def gen_label(val_txt_path):
-    true_label = []
-    val_txt = np.loadtxt(val_txt_path, dtype = str)
-    for idx, name in enumerate(val_txt):
-        label = int(name.split('A')[1][:3])
-        true_label.append(label)
-
-    true_label = torch.from_numpy(np.array(true_label))
-    return true_label
 
 if __name__ == "__main__":
-    parser = get_parser()
-    args = parser.parse_args()
-    
-    # Mix_GCN Score File
-    j_file = args.mixformer_J_Score
-    b_file = args.mixformer_B_Score
-    jm_file = args.mixformer_JM_Score
-    bm_file = args.mixformer_BM_Score
-    k2_file = args.mixformer_k2_Score
-    #k2m_file = args.mixformer_k2M_Score
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset',
+                        required=True,
+                        choices={'csv1','csv2'},
+                        help='the work folder for storing results')
+    parser.add_argument('--alpha',
+                        default=1,
+                        help='weighted summation',
+                        type=float)
 
-    
-    val_txt_file = args.val_sample
+    parser.add_argument('--joint-dir',
+                        help='Directory containing "epoch1_test_score.pkl" for joint eval results')
+    parser.add_argument('--bone-dir',
+                        help='Directory containing "epoch1_test_score.pkl" for bone eval results')
+    parser.add_argument('--joint-motion-dir', default=None)
+    parser.add_argument('--bone-motion-dir', default=None)
+    parser.add_argument('--joint-k2-dir', default=None)
+    parser.add_argument('--joint-motion-k2-dir', default=None)
+    parser.add_argument('--joint-dir2',
+                        help='Directory containing "epoch1_test_score.pkl" for joint eval results')
+    parser.add_argument('--bone-dir2',
+                        help='Directory containing "epoch1_test_score.pkl" for bone eval results')
+    parser.add_argument('--joint-motion-dir2', default=None)
+    parser.add_argument('--bone-motion-dir2', default=None)
 
-    File = [j_file, b_file, jm_file, bm_file, k2_file]#, k2m_file]    
-    if args.benchmark == 'V1':
-        Numclass = 155
-        Sample_Num = 6307
-        Rate = [0.7, 0.7, 0.3, 0.3, 0.3]#, 0.3] 
-        final_score = Cal_Score(File, Rate, Sample_Num, Numclass)
-        true_label = gen_label(val_txt_file)
-    
-    if args.benchmark == 'V2':
-        Numclass = 155
-        Sample_Num = 6599
-        Rate = [0.7, 0.7, 0.3, 0.3, 0.3]#, 0.3]  
-        final_score = Cal_Score(File, Rate, Sample_Num, Numclass)
-        true_label = gen_label(val_txt_file)
-    
-    Acc = Cal_Acc(final_score, true_label)
 
-    print('acc:', Acc)
+    arg = parser.parse_args()
+
+    dataset = arg.dataset
+    if 'csv1' in arg.dataset:
+        npz_data = np.load('/data/liujinfu/icmew/pose_data/V1.npz')
+        label = npz_data['y_test']#np.where(npz_data['y_test'] > 0)[1]
+    elif 'csv2' in arg.dataset:
+        npz_data = np.load('/data/liujinfu/icmew/pose_data/V2.npz')
+        label = npz_data['y_test']#np.where(npz_data['y_test'] > 0)[1]
+
+    else:
+        raise NotImplementedError
+
+    with open(os.path.join(arg.joint_dir, 'epoch1_test_score.pkl'), 'rb') as r1:
+        r1 = list(pickle.load(r1).items())
+
+    with open(os.path.join(arg.bone_dir, 'epoch1_test_score.pkl'), 'rb') as r2:
+        r2 = list(pickle.load(r2).items())
+
+    if arg.joint_motion_dir is not None:
+        with open(os.path.join(arg.joint_motion_dir, 'epoch1_test_score.pkl'), 'rb') as r3:
+            r3 = list(pickle.load(r3).items())
+    if arg.bone_motion_dir is not None:
+        with open(os.path.join(arg.bone_motion_dir, 'epoch1_test_score.pkl'), 'rb') as r4:
+            r4 = list(pickle.load(r4).items())
+
+    if arg.joint_k2_dir is not None:
+        with open(os.path.join(arg.joint_k2_dir, 'epoch1_test_score.pkl'), 'rb') as r5:
+            r5 = list(pickle.load(r5).items())
+    if arg.joint_motion_k2_dir is not None:
+        with open(os.path.join(arg.joint_motion_k2_dir, 'epoch1_test_score.pkl'), 'rb') as r6:
+            r6 = list(pickle.load(r6).items())
+
+    with open(os.path.join(arg.joint_dir2, 'epoch1_test_score.pkl'), 'rb') as r7:
+        r7 = list(pickle.load(r7).items())
+
+    with open(os.path.join(arg.bone_dir2, 'epoch1_test_score.pkl'), 'rb') as r8:
+        r8 = list(pickle.load(r8).items())
+
+    if arg.joint_motion_dir is not None:
+        with open(os.path.join(arg.joint_motion_dir2, 'epoch1_test_score.pkl'), 'rb') as r9:
+            r9 = list(pickle.load(r9).items())
+    if arg.bone_motion_dir is not None:
+        with open(os.path.join(arg.bone_motion_dir2, 'epoch1_test_score.pkl'), 'rb') as r10:
+            r10 = list(pickle.load(r10).items())
+            
+    if arg.joint_motion_dir is not None and arg.bone_motion_dir is not None:
+        space = [(0.2, 1.2) for i in range(10)]
+        result = gp_minimize(objective, space, n_calls=200, random_state=0)
+        print('Maximum accuracy: {:.4f}%'.format(-result.fun * 100))
+        print('Optimal weights: {}'.format(result.x))
+
+
+
